3942554b58bf272abcfddb4dc38ff1ede36f7906
Czerny-F <dummy00.cz@gmail.com>
2017-04-24 11:47:07 +0900

    coderep2

---
+++ src/quotations.scala
@@ -0,0 +1,13 @@
+val name = "World"
+val num = 1
+val zero = 0
+
+println(s"${zero + num}: Hello ${name}!")
+
+val text = s"""${num + 1}: Object-Oriented Meets Functional
+              |Have the best of both worlds. Construct elegant class
+              |hierarchies for maximum code reuse and extensibility,
+              |implement their behavior using higher-order functions.
+              |Or anything in-between.""".stripMargin
+
+println(text)
