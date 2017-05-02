5453bd70ebcd89d8b96c384f48ebd6fff913d18e
Czerny-F <dummy00.cz@gmail.com>
2017-05-02 13:19:38 +0900

    modified Closure

--- src/Closure.scala
+++ src/Closure.scala
@@ -1,10 +1,14 @@
 object Closure {
   def main(args: Array[String]):Unit = {
     var candidates = List(1,2,3,4,5)
+
     val filter = (predicate: Int => Boolean) => {
+      println("filter executed")
       candidates = candidates :+ 6
 
-      (candidates: List[Int]) => {
+      // (candidates: List[Int]) => {
+        // for (x <- candidates; if predicate(x)) yield x
+      () => {
         for (x <- candidates; if predicate(x)) yield x
       }:List[Int]
     }
@@ -12,9 +16,9 @@ object Closure {
     val predicate = (x: Int) => x % 2 == 0
 
     var oodFilter = filter(predicate)
-    oodFilter(candidates).foreach(println)
-    oodFilter(candidates).foreach(println)
-    // filter(predicate)().foreach(println)
-    // filter(predicate)().foreach(println)
+    // oodFilter(candidates).foreach(println)
+    // oodFilter(candidates).foreach(println)
+    oodFilter().foreach(println)
+    oodFilter().foreach(println)
   }
 }
